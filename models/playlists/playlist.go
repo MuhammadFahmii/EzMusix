cab8b06c4d3814e12beb16fcc923b9ce3593dc71
MuhammadFahmii <fahmiprasetio754@gmail.com>
2021-10-02 16:18:26 +0700

    fix table name

--- models/playlists/playlist.go
+++ models/playlists/playlist.go
@@ -6,5 +6,5 @@ type Playlist struct {
 	Id     int            `gorm:"primaryKey" json:"id"`
 	Name   string         `json:"name" form:"name"`
 	UserID int            `json:"user_id" form:"user_id"`
-	Tracks []tracks.Track `gorm:"many2many:detail_playlist constraint:OnDelete:CASCADE"`
+	Tracks []tracks.Track `gorm:"many2many:detail_playlist" constraint:"OnDelete:CASCADE"`
 }
