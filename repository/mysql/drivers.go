0d2a56bee8f7f35c905be62a8e96b315a4c569e4
MuhammadFahmii <fahmiprasetio754@gmail.com>
2021-10-03 17:04:27 +0700

    add gitignore

---
+++ repository/mysql/drivers.go
@@ -0,0 +1,38 @@
+package mysql
+
+import (
+	"EzMusix/repository/mysql/playlist"
+	"EzMusix/repository/mysql/users"
+	"EzMusix/repository/thirdparty"
+	"fmt"
+
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+
+	config := map[string]string{
+		"DB_Username": viper.GetString("database.username"),
+		"DB_Password": viper.GetString("database.password"),
+		"DB_Port":     viper.GetString("database.port"),
+		"DB_Host":     viper.GetString("database.host"),
+		"DB_Name":     viper.GetString("database.name"),
+	}
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", config["DB_Username"], config["DB_Password"], config["DB_Host"], config["DB_Port"], config["DB_Name"])
+
+	var err error
+	DB, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	DB.AutoMigrate(
+		&users.User{},
+		&playlist.Playlist{},
+		&thirdparty.Track{},
+	)
+
+	return DB
+}
