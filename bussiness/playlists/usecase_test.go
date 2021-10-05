0ad707fc6f5dbe75cc412e7b8f0a5a5972f3c445
MuhammadFahmii <fahmiprasetio754@gmail.com>
2021-10-05 08:15:15 +0700

    adding some unit test

--- bussiness/playlists/usecase_test.go
+++ bussiness/playlists/usecase_test.go
@@ -3,8 +3,10 @@ package playlists_test
 import (
 	"EzMusix/bussiness/playlists"
 	"EzMusix/bussiness/playlists/mocks"
+	"errors"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -23,7 +25,12 @@ func testSetup() {
 
 func TestInsert(t *testing.T) {
 	testSetup()
-	t.Run("Test Case 1 | Valid Data", func(t *testing.T) {
+	t.Run("Test Case 1 | Data not valid", func(t *testing.T) {
+		playlistsRepo.On("Insert", mock.Anything).Return(playlists.Domain{}, errors.New("Data not valid")).Once()
+		_, err := playlistUseCase.Insert(playlistDomain)
+		assert.NotEqual(t, nil, err, "Please input valid data")
+	})
+	t.Run("Test Case 2 | Valid Data", func(t *testing.T) {
 		playlistsRepo.On("Insert", mock.Anything).Return(playlistDomain, nil).Once()
 		playlistUseCase.Insert(playlistDomain)
 	})
@@ -31,7 +38,12 @@ func TestInsert(t *testing.T) {
 
 func TestGet(t *testing.T) {
 	testSetup()
-	t.Run("Test Case 1 | Valid Data", func(t *testing.T) {
+	t.Run("Test Case 1 | Data Empty", func(t *testing.T) {
+		playlistsRepo.On("Get", mock.Anything).Return([]playlists.Domain{}, errors.New("Data empty")).Once()
+		_, err := playlistUseCase.Get(playlistDomain)
+		assert.NotEqual(t, nil, err, "Data should not be empty")
+	})
+	t.Run("Test Case 2 | Data Found", func(t *testing.T) {
 		playlistsRepo.On("Get", mock.Anything).Return([]playlists.Domain{}, nil).Once()
 		playlistUseCase.Get(playlistDomain)
 	})
@@ -39,7 +51,12 @@ func TestGet(t *testing.T) {
 
 func TestDelete(t *testing.T) {
 	testSetup()
-	t.Run("Test Case 1 | Valid Data", func(t *testing.T) {
+	t.Run("Test Case 1 | Data not found", func(t *testing.T) {
+		playlistsRepo.On("Delete", mock.Anything).Return(playlists.Domain{}, errors.New("Data not found")).Once()
+		_, err := playlistUseCase.Delete(playlistDomain)
+		assert.NotEqual(t, nil, err, "Please input valid data")
+	})
+	t.Run("Test Case 2 | Data Found", func(t *testing.T) {
 		playlistsRepo.On("Delete", mock.Anything).Return(playlistDomain, nil).Once()
 		playlistUseCase.Delete(playlistDomain)
 	})
